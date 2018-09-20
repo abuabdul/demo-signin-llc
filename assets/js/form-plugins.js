544170bb544058231068e68bfa52e59e33195520
Abubacker Siddik <abubacker@qburst.com>
2018-09-20 17:03:12 +0530

    Add html files for signin llc

---
+++ assets/js/form-plugins.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    // Bootstrap datepicker
+    $('#date_1 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+    $('#date_2 .input-group.date').datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+    });
+
+});
